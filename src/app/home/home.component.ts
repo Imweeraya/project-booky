4f3f3d4ed4dfa124ea6c5d358f45c0c93620d40d
Imweeraya <weerayakosaldilok@gmail.com>
2023-08-30 13:36:09 +0700

    add promote area

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   constructor() { }
-    topsale = true;
+    current_img = 1;
+    //srcimage = "../assets/img/cat1.jpg";
+
+    change_img_plus(){
+      if(this.current_img<3){
+        this.current_img++
+      }else{
+        this.current_img  = 3
+      }
+    }
+
+    change_img_minus(){
+      if(this.current_img>1){
+        this.current_img--
+      }else{
+        this.current_img  = 1
+      }
+    }
     
 }
