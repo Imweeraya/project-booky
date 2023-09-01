b436613103695833922005289498ff2b1b442078
Imweeraya <weerayakosaldilok@gmail.com>
2023-09-01 23:40:12 +0700

    update cart component

--- src/app/home/cart.component.ts
+++ src/app/home/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'cart',
@@ -23,4 +24,25 @@ export class Cart {
       this.quantity--;
     }
   }
+
+  deleteProduct() {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+  }
+
 }
