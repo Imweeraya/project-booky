8df679d4288b2ef784261d1967bb42cbbafe2e6a
Imweeraya <weerayakosaldilok@gmail.com>
2023-09-03 23:53:23 +0700

    add changepage button

--- src/app/home/bookpage.component.ts
+++ src/app/home/bookpage.component.ts
@@ -8,47 +8,63 @@ import { ProductDataService } from '../product-data.service';
   templateUrl: './bookpage.component.html',
 })
 export class Bookpage {
-
-  selectedCategory:string;
-  selectedgenre:string;
-  productsPerPage:number;
-  selectedPage:number;
-  productID:number;
+  selectedCategory: string;
+  selectedgenre: string;
+  productsPerPage: number;
+  selectedPage: number;
+  productID: number;
 
   constructor(
     private productDataService: ProductDataService,
-    private repository: ProductRepository,
+    private repository: ProductRepository
   ) {
     this.productID = 0;
     this.selectedPage = 1;
     this.productsPerPage = 8;
-    this.selectedgenre = "all";
-    this.selectedCategory = "หนังสือ";
+    this.selectedgenre = 'all';
+    this.selectedCategory = 'หนังสือ';
   }
 
   get products(): Product[] {
-    this.selectedPage = 1;
-    if(this.selectedgenre=="all"){
-      return this.repository.getProductsCategory(this.selectedCategory);
-    }else{
-      return this.repository.getProductsGenre(this.selectedgenre);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+
+    if (this.selectedgenre === 'all') {
+      return this.repository
+        .getProductsCategory(this.selectedCategory)
+        .slice(pageIndex, pageIndex + this.productsPerPage);
+    } else {
+      return this.repository
+        .getProductsGenre(this.selectedgenre)
+        .slice(pageIndex, pageIndex + this.productsPerPage);
     }
+  }
+
+  get pageNumbers(): number[] {
+    let totalProducts: Product[];
+    
+    if (this.selectedgenre === 'all') {
+      totalProducts = this.repository.getProductsCategory(this.selectedCategory);
+    } else {
+      totalProducts = this.repository.getProductsGenre(this.selectedgenre);
+    }
+  
+    const totalPages = Math.ceil(totalProducts.length / this.productsPerPage);
+    console.log('Total pages:', totalPages);
+    return Array(totalPages).fill(0).map((x, i) => i + 1);
+  }
+  
+
+  changePage(newPage: number) {
+    console.log('Changing to page:', newPage);
+    this.selectedPage = newPage;
   }
 
   // changeGenre(newGenre?: string) {
   //   this.selectedgenre = newGenre;
   // }
 
-  changePage(newPage: number) {
-    this.selectedPage = newPage;
-  }
-
-
   setID(productId?: number) {
-  this.productID = productId !== undefined ? productId : 0; // Check for undefined
-  this.productDataService.setProductId(this.productID); // Call the setProductId method with the value
-}
-
-  
-
+    this.productID = productId !== undefined ? productId : 0; // Check for undefined
+    this.productDataService.setProductId(this.productID); // Call the setProductId method with the value
+  }
 }
